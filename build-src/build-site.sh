0d9dd0eaefab994a3241940a58b4d8cd9bf0924f
Router Man <johns@debian>
2024-08-31 13:02:53 -0500

    added templating

--- build-src/build-site.sh
+++ build-src/build-site.sh
@@ -6,5 +6,6 @@ find ./site-src -name '*.md' -print0 |
     while IFS= read -r -d '' infile; do
        outfile="./site-dest/`cut -d'/' -f3- <<< "${infile%.*}.html"`"
        echo "placing file in $outfile"
-       pandoc --verbose -f markdown -t html5 -o ${outfile} ${infile}
+       pandoc ${infile} -f markdown -o ${outfile} --template=./site-src/templates/easy_template.html --toc
+       # pandoc --verbose -f markdown -t html5 -o ${outfile} ${infile}
     done
