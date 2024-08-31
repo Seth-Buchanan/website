5ef571cec9fbcdba285618c841f8b7f16376857a
Router Man <johns@debian>
2024-08-31 18:12:21 -0500

    Cleaning and images

--- build-src/build-site.sh
+++ build-src/build-site.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-find ./site-dest/ -name "*.html" -exec rm {} \;
+#Cleaning up the dest directory
+rm -rf ./site-dest/*
+mkdir ./site-dest/images
 
 template=`realpath site-src/template/easy_template.html`
 
@@ -9,5 +11,14 @@ find ./site-src -name '*.md' -print0 |
 	name=`cut -d'/' -f3- <<< "${infile%.*}"`
 	outfile="./site-dest/${name}.html"
 	pandoc ${infile} -f markdown -o ${outfile} --template=${template} --toc --metadata title="${name}"
-	# pandoc --verbose -f markdown -t html5 -o ${outfile} ${infile}
     done
+
+find ./site-src/images -mindepth 1 -type d -print0 |
+    while IFS= read -r -d '' directory; do
+	mkdir "./site-dest/${directory#*/*/}"
+    done	
+
+find ./site-src/images -mindepth 1 -type f -print0 |
+    while IFS= read -r -d '' image; do
+	ln -s image "./site-dest/${image#*/*/}"
+    done	
