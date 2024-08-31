498c4e4edace0a18c5d09bb6a80f6c24ce7d3dbd
Router Man <johns@debian>
2024-08-31 13:35:24 -0500

    fixed templating

--- build-src/build-site.sh
+++ build-src/build-site.sh
@@ -2,10 +2,12 @@
 
 find ./site-dest/ -name "*.html" -exec rm {} \;
 
+template=`realpath site-src/template/easy_template.html`
+
 find ./site-src -name '*.md' -print0 |
     while IFS= read -r -d '' infile; do
-       outfile="./site-dest/`cut -d'/' -f3- <<< "${infile%.*}.html"`"
-       echo "placing file in $outfile"
-       pandoc ${infile} -f markdown -o ${outfile} --template=./site-src/templates/easy_template.html --toc
-       # pandoc --verbose -f markdown -t html5 -o ${outfile} ${infile}
+	name=`cut -d'/' -f3- <<< "${infile%.*}"`
+	outfile="./site-dest/${name}.html"
+	pandoc ${infile} -f markdown -o ${outfile} --template=${template} --toc --metadata title="${name}"
+	# pandoc --verbose -f markdown -t html5 -o ${outfile} ${infile}
     done
