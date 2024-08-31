1823c2366767f0f8d1d9f3b621bd89ea08a7c539
Router Man <johns@debian>
2024-08-31 12:29:14 -0500

    moved site root

--- build-src/build-site.sh
+++ build-src/build-site.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-rm ./build-dest/*.html
+find ./site-dest/ -name "*.html" -exec rm {} \;
 
 find ./site-src -name '*.md' -print0 |
-    while IFS= read -r -d '' input; do
-       outfile="./build-dest/`cut -d'/' -f3- <<< "${input%.*}.html"`"
+    while IFS= read -r -d '' infile; do
+       outfile="./site-dest/`cut -d'/' -f3- <<< "${infile%.*}.html"`"
        echo "placing file in $outfile"
-       pandoc --verbose -f markdown -t html5 -o ${outfile} ${input}
+       pandoc --verbose -f markdown -t html5 -o ${outfile} ${infile}
     done
