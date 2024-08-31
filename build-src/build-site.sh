a312c69187f5ecc10b7025f8ff50ca55aae6b11e
Router Man <johns@debian>
2024-08-31 11:18:02 -0500

    changed dest-directory

--- build-src/build-site.sh
+++ build-src/build-site.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-rm ./root/*.html
+
+rm ./build-dest/*.html
 
 find ./site-src -name '*.md' -print0 |
     while IFS= read -r -d '' input; do
-       outfile="./root/`cut -d'/' -f3- <<< "${input%.*}.html"`"
+       outfile="./build-dest/`cut -d'/' -f3- <<< "${input%.*}.html"`"
        echo "placing file in $outfile"
        pandoc --verbose -f markdown -t html5 -o ${outfile} ${input}
     done
