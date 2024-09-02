90a9d4f2464f2122003987f97e132a27b9cf35d7
Router Man <johns@debian>
2024-09-02 13:00:51 -0500

    shorted text subsitutions

--- build-site.sh
+++ build-site.sh
@@ -2,21 +2,20 @@
 
 #Cleaning up the dest directory
 rm -rf ./site-dest/*
-ls -alh site-dest
-
-template=`realpath site-src/template/easy_template.html`
-
-find ./site-src -name '*.md' -print0 |
-    while IFS= read -r -d '' infile; do
-	name=`cut -d'/' -f3- <<< "${infile%.*}"`
-	outfile="./site-dest/${name}.html"
-	pandoc ${infile} -f markdown -o ${outfile} --template=${template} --toc
-    done
 
 find ./site-src -mindepth 1 -type d -print0 |
     while IFS= read -r -d '' directory; do
 	mkdir "./site-dest/${directory#*/*/}"
     done	
+
+template=`realpath site-src/template/easy_template.html`
+
+find ./site-src -name '*.md' -print0 |
+    while IFS= read -r -d '' infile; do
+	name="${infile%.*}"
+	outfile="./site-dest/${name#*/*/}.html"
+	pandoc ${infile} -f markdown -o ${outfile} --template=${template} --toc
+    done
 
 find ./site-src/images -mindepth 1 -type f -print0 |
     while IFS= read -r -d '' image; do
