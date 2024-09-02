b67418acb81b5d1c658b376ae9eb4dfb3008d490
Router Man <johns@debian>
2024-09-02 13:32:09 -0500

    added dest-dir

--- build-site.sh
+++ build-site.sh
@@ -3,18 +3,19 @@
 #Cleaning up the dest directory
 rm -rf ./site-dest/*
 
-find ./site-src -mindepth 1 -type d -print0 |
+find ./site-src  -mindepth 1 -not -path "./site-src/templates" -type d -print0 |
     while IFS= read -r -d '' directory; do
 	mkdir "./site-dest/${directory#*/*/}"
     done	
 
-template=`realpath site-src/template/easy_template.html`
+template=`realpath site-src/templates/easy_template.html`
 
 find ./site-src -name '*.md' -print0 |
     while IFS= read -r -d '' infile; do
 	name="${infile%.*}"
 	outfile="./site-dest/${name#*/*/}.html"
-	pandoc ${infile} -f markdown -o ${outfile} --template=${template} --toc
+	pandoc ${infile} -f markdown -o ${outfile} --template=${template} --toc \
+	       --strip-comments --email-obfuscation=references
     done
 
 find ./site-src/images -mindepth 1 -type f -print0 |
