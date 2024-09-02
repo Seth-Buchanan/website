39c1dbb44a23f5e39c376dc7ae1284bd6e7fc4e7
Router Man <johns@debian>
2024-09-02 11:33:30 -0500

    messing with symlinks

--- build-site.sh
+++ build-site.sh
@@ -13,6 +13,13 @@ find ./site-src -name '*.md' -print0 |
 	pandoc ${infile} -f markdown -o ${outfile} --template=${template} --toc --metadata title="${name}"
     done
 
+find ./site-src -name '*.org' -print0 |
+    while IFS= read -r -d '' infile; do
+	name=`cut -d'/' -f3- <<< "${infile%.*}"`
+	outfile="./site-dest/${name}.html"
+	pandoc ${infile} -f org -o ${outfile} --template=${template} --toc --metadata title="${name}"
+    done
+
 find ./site-src/images -mindepth 1 -type d -print0 |
     while IFS= read -r -d '' directory; do
 	mkdir "./site-dest/${directory#*/*/}"
@@ -20,5 +27,7 @@ find ./site-src/images -mindepth 1 -type d -print0 |
 
 find ./site-src/images -mindepth 1 -type f -print0 |
     while IFS= read -r -d '' image; do
-	ln -s image "./site-dest/${image#*/*/}"
+	echo $image
+	echo "./site-dest/${image#*/*/}"
+	ln -rs $image "../../site-dest/${image#*/*/}"
     done	
